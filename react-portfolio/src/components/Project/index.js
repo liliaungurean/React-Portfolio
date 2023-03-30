8f6b5280f10e9ad236df2cd66dd68db33fdfc617
liliaungurean <lilicikau95@gmail.com>
2023-03-30 00:33:19 -0500

    stying

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -22,7 +22,7 @@ const Projects = ({ singleProject }) => {
     <Card
       style={{
         width: "20rem",
-        margin: "20px 50px 20px 10px", backgroundColor: "#EAF4D3" 
+        margin: "20px 50px 20px 10px", backgroundColor: "#A9A9A9" 
       }}
       key={singleProject.title} className="col-3"
     >
@@ -30,22 +30,22 @@ const Projects = ({ singleProject }) => {
       <Card.Title style={{ margin: "15px", fontSize: "27px", color: "#094067"}}>
         {singleProject.title}
       </Card.Title>
-      <Card.Text className="mb-2 text-muted" style={{ fontSize: "15px", margin: "20px 0px 20px 10px" }}>{singleProject.summary}</Card.Text>
+      <Card.Text className="mb-2 text-white" style={{ fontSize: "15px", margin: "20px 0px 20px 10px" }}>{singleProject.summary}</Card.Text>
       <Card.Img
         variant="top"
         src={singleProject.image}
         alt={singleProject.title}
         style={{ position: "relative" }}
       />
-      <Card.Body style={{ backgroundColor: "#EAF4D3"}}>
-        <Card.Text style={{ fontSize: "17px" }} className="text-muted">
+      <Card.Body style={{ backgroundColor: "#A9A9A9"}}>
+        <Card.Text style={{ fontSize: "17px" }} className="text-black">
           {"Built with: "}
           <Card.Text style={{ fontSize: "15px" }} className="text-muted">{singleProject.technologies}</Card.Text>
         </Card.Text>
       </Card.Body>
       <Card.Footer
         style={{
-          backgroundColor: "#EAF4D3",
+          backgroundColor: "#A9A9A9",
           padding: "5px 20px 5px 20px",
         }}
       >
